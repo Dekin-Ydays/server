d13de835656bca5ded9b39bc2c603cea7b70e885
Quantix06 <marchalquentin06@gmail.com>
2025-01-31 16:06:46 +0100

    Initial commit

---
+++ apifilrouge/src/main/java/com/projetfilrougeapi/apifilrouge/ApifilrougeApplication.java
@@ -0,0 +1,13 @@
+package com.projetfilrougeapi.apifilrouge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApifilrougeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApifilrougeApplication.class, args);
+	}
+
+}
---
+++ apifilrouge/src/test/java/com/projetfilrougeapi/apifilrouge/ApifilrougeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.projetfilrougeapi.apifilrouge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApifilrougeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
