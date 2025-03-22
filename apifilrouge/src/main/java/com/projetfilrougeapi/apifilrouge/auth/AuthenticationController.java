5be70e9aaf3a3bc8d49a70f733b01b040096c5bf
Quantix06 <marchalquentin06@gmail.com>
2025-03-22 15:39:50 +0100

    Add Event entity and repository, and update User repository for REST support

--- apifilrouge/src/main/java/com/projetfilrougeapi/apifilrouge/auth/AuthenticationController.java
+++ apifilrouge/src/main/java/com/projetfilrougeapi/apifilrouge/auth/AuthenticationController.java
@@ -1,17 +1,24 @@
 package com.projetfilrougeapi.apifilrouge.auth;
 
+/*import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.NoArgsConstructor;*/
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.io.IOException;
+
 @RestController
 @RequestMapping("/api/v1/auth")
 @RequiredArgsConstructor
-public class AuthenticationController {
 
+public class AuthenticationController {
+    
 
     private final AuthenticationService service;
 
@@ -24,4 +31,11 @@ public class AuthenticationController {
     public ResponseEntity<AuthenticationResponse> authenticate(@RequestBody AuthenticationRequest request) {
         return ResponseEntity.ok(service.authenticate(request));
     }
+    /*@PostMapping("/refresh-token")
+    public void refreshToken(
+            HttpServletRequest request,
+            HttpServletResponse response
+    ) throws IOException {
+        service.refreshToken(request, response);
+    }*/
 }
