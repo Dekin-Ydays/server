4772714615f475ce1ed34ddafa347c8d1b95bc96
Sclavo Morgan <132832713+ElChip21@users.noreply.github.com>
2025-06-14 19:42:46 +0200

    add of modif in the CORS to use the api correctly

--- apifilrouge/src/main/java/com/projetfilrougeapi/apifilrouge/config/SecurityConfiguration.java
+++ apifilrouge/src/main/java/com/projetfilrougeapi/apifilrouge/config/SecurityConfiguration.java
@@ -43,7 +43,7 @@ public class SecurityConfiguration {
         configuration.setMaxAge(3600L);
 
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-        source.registerCorsConfiguration("/api/**", configuration);
+        source.registerCorsConfiguration("/**", configuration);
         return source;
     }
 
